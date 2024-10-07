0c2a86e765bc4e326ed346daccdf089ebf7e300c
Cesar Trigo <cesar.luis-trigo@hotmail.com>
2024-10-07 15:31:16 -0300

    form de registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./views/Home/Home";
+import LayoutMain from "./layout/LayoutMain";
+import Auth from "./views/Auth/Auth";
+import Login from "./views/Auth/Login/Login";
+import Register from "./views/Auth/Register/register";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LayoutMain />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />}>
+            <Route path="/auth/login" element={<Login />} />
+            <Route path="/auth/register" element={<Register />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/views/Home/Home.jsx
@@ -0,0 +1,6 @@
+import style from "./Home.module.css";
+
+function Home() {
+  return <main className={style.home}>Home</main>;
+}
+export default Home;
